90855bc97828982cf11116ee22592946791f2f5c
Juan Manuel <juanmavazquez91@gmail.com>
2023-05-19 23:15:08 -0300

    hash

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -18,8 +18,7 @@ export const createUserAdmin = async (req, res) => {
         const isAdmin = true
         const manager = new UsersManager();
         const newUser = await manager.createUser(data, isAdmin)
-        console.log(newUser)
-        return res.status(200).send({ status: "success", message: `Administrador "${newUser.email}" creado`, payload: payload: {...newUser, password: undefined }})
+        return res.status(200).send({ status: "success", message: `Administrador "${newUser.email}" creado`, payload: {...newUser, password: undefined }})
     }
     catch (e) {
         res.status(400).send({ status: "error", message: e.message });
