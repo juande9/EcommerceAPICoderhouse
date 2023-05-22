d08d4403b7bc3db2594d38b369fd3fcaabfba3b1
Juan Manuel <juanmavazquez91@gmail.com>
2023-05-22 11:52:13 -0300

    refactor passport

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -15,6 +15,16 @@ export const login = async (req, res) => {
     }
 }
 
+export const loginPassport = async (req, res) => {
+    if (!req.user) res.status(400).send({ status: 'error', message: 'Invalid credentials' })
+
+    req.session.passport = {
+        email: req.user.email,
+    }
+
+    res.send({ status: 'success', message: `${req.user.email} log in` })
+}
+
 
 export const signup = async (req, res) => {
     try {
@@ -22,23 +32,37 @@ export const signup = async (req, res) => {
         const manager = new SessionManager();
         const newUser = await manager.signup(data)
 
-        res.status(201).send({ status: "success", message: `Usuario ${newUser.email} creado`, payload: {...newUser, password: undefined }})
+        res.status(201).send({ status: "success", message: `Usuario ${newUser.email} creado`, payload: { ...newUser, password: undefined } })
     }
     catch (e) {
         res.status(400).send({ status: "error", message: e.message });
     }
 }
 
+export const signupPassport = async (req, res) => {
+
+    const newUser = req.user
+    res.status(201).send({
+        status: 'success', message: `${newUser.email} Registered`,
+        payload: { ...newUser, password: undefined }
+    })
+}
+
 
 export const logout = (req, res) => {
     try {
         req.session.destroy(err => {
+            req.logout(); // Cerrar sesión en Passport
             if (!err) {
-                return res.status(201).send({ status: "success", message: "Usuario deslogueado." });
+                return res.status(201).send({ status: "success", message: "Logout successful." });
             }
         })
     }
     catch (e) {
         res.status(400).send({ status: "error", message: "Log out error" });
     }
-} 
+}
+
+export const fail = async (req, res) => {
+    res.status(400).send({ status: "error", message: "Proccess Failed." });
+}
--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, logout, signup } from '../controllers/sessionController.js';
+import { login, logout, signup, signupPassport, loginPassport, fail } from '../controllers/sessionController.js';
 import passport from 'passport';
 
 const sessionRouter = Router()
@@ -7,23 +7,8 @@ const sessionRouter = Router()
 sessionRouter.post("/login", login)
 sessionRouter.post("/logout", logout)
 sessionRouter.post("/signup", signup)
-sessionRouter.post('/registerPassport', passport.authenticate('register', { failureRedirect: 'failRegister' }), async (req, res) => {
-    const newUser = req.user
-    res.status(201).send({ status: 'success', message: `${newUser.email} Registered`, payload: { ...newUser, password: undefined } })
-})
-sessionRouter.post('/loginPassport', passport.authenticate('login', { failureRedirect: 'failRegister' }), async (req, res) => {
-    if (!req.user) res.status(400).send({ status: 'error', message: 'Invalid credentials' })
-
-    req.session.user = {
-        email: req.user.email,
-    }
-
-    res.send({ status: 'success', message: `${req.user.email} log in` })
-})
-sessionRouter.get('/failRegister', async (req, res) => {
-    console.log('Failed registration')
-    res.send({ error: 'Failed' })
-})
-
+sessionRouter.post('/registerPassport', passport.authenticate('register', { failureRedirect: 'failPassport' }), signupPassport)
+sessionRouter.post('/loginPassport', passport.authenticate('login', { failureRedirect: 'failPassport' }), loginPassport)
+sessionRouter.get('/failPassport', fail)
 
 export default sessionRouter
